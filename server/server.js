7203c4f247fd60309eb8d214e2c84310eeb39c77
Denish Jobanputra <jobanputradenishn1809@gmail.com>
2026-08-07 17:14:11 +0530

    Improve Server configuration for the production

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+dotenv.config();
+
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import testRoutes from './routes/testRoutes.js';
@@ -9,6 +12,8 @@ import attendanceRoutes from './routes/attendanceRoutes.js';
 import marksRoutes from './routes/markRoutes.js';
 import analyticsRoutes from './routes/analyticsRoutes.js';
 import subjectRoutes from './routes/subjectRoutes.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 connectDB();
 
@@ -17,7 +22,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true
+  })
+);
+
+app.use(helmet());
+app.use(morgan('dev'));
+
 app.use('/api/auth',authRoutes);
 app.use('/api/test',testRoutes);
 app.use('/api/students',studentRoutes);
@@ -26,10 +41,14 @@ app.use('/api/marks',marksRoutes);
 app.use('/api/analytics',analyticsRoutes);
 app.use('/api/subjects',subjectRoutes);
 
-dotenv.config();
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to the Authentication API');
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Classroom Analytics Dashboard API",
+    version: "1.0.0",
+    status: "Running"
+  });
 });
 
 app.listen(PORT,()=>{
